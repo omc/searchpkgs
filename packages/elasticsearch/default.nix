b5a22d5a0b1050499afe204fc111aba61302ac3b
Brian McGee <brian@bmcgee.ie>
2024-05-21 11:50:42 +0100

    fix(elasticsearch): adjust package based on version

--- packages/elasticsearch/default.nix
+++ packages/elasticsearch/default.nix
@@ -14,67 +14,76 @@
   coreutils,
   gnugrep,
   autoPatchelfHook,
-}:
-stdenv.mkDerivation {
-  inherit pname version;
+  libxcrypt-legacy,
+  fixDarwinDylibNames,
+}: let
+  applyPatch = (builtins.compareVersions version "6.4.0") >= 0;
+in
+  stdenv.mkDerivation {
+    inherit pname version;
 
-  src = fetchurl {
-    inherit url sha256;
-  };
+    src = fetchurl {
+      inherit url sha256;
+    };
 
-  patches = [./es-home-6.x.patch];
+    patches = lib.optionals applyPatch [./es-home-6.x.patch];
 
-  postPatch = ''
-    substituteInPlace bin/elasticsearch-env --replace \
-      "ES_CLASSPATH=\"\$ES_HOME/lib/*\"" \
-      "ES_CLASSPATH=\"$out/lib/*\""
+    postPatch = ''
+      if [ -e bin/elasticsearch-env ]; then
+          substituteInPlace bin/elasticsearch-env --replace \
+            "ES_CLASSPATH=\"\$ES_HOME/lib/*\"" \
+            "ES_CLASSPATH=\"$out/lib/*\""
+      fi
 
-    substituteInPlace bin/elasticsearch-cli --replace \
-      "ES_CLASSPATH=\"\$ES_CLASSPATH:\$ES_HOME/\$additional_classpath_directory/*\"" \
-      "ES_CLASSPATH=\"\$ES_CLASSPATH:$out/\$additional_classpath_directory/*\""
-  '';
+      if [ -e bin/elasticsearch-cli ]; then
+        substituteInPlace bin/elasticsearch-cli --replace \
+          "ES_CLASSPATH=\"\$ES_CLASSPATH:\$ES_HOME/\$additional_classpath_directory/*\"" \
+          "ES_CLASSPATH=\"\$ES_CLASSPATH:$out/\$additional_classpath_directory/*\""
+      fi
+    '';
 
-  nativeBuildInputs =
-    [makeWrapper]
-    ++ lib.optional (!stdenv.hostPlatform.isDarwin) autoPatchelfHook;
+    nativeBuildInputs =
+      [makeWrapper]
+      ++ lib.optional stdenv.hostPlatform.isLinux autoPatchelfHook
+      ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
 
-  buildInputs = [jdk17 util-linux zlib];
+    buildInputs = [jdk17 util-linux zlib libxcrypt-legacy];
 
-  runtimeDependencies = [zlib];
+    runtimeDependencies = [zlib];
 
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
 
-    ls -alr plugins
-    mkdir -p $out
-    cp -R bin config lib modules plugins $out
+      ls -alr plugins
+      mkdir -p $out
+      cp -R bin config lib modules plugins $out
 
-    chmod +x $out/bin/*
+      chmod +x $out/bin/*
 
-    substituteInPlace $out/bin/elasticsearch \
-      --replace 'bin/elasticsearch-keystore' "$out/bin/elasticsearch-keystore"
+      substituteInPlace $out/bin/elasticsearch \
+        --replace 'bin/elasticsearch-keystore' "$out/bin/elasticsearch-keystore"
 
-    wrapProgram $out/bin/elasticsearch \
-      --prefix PATH : "${lib.makeBinPath [util-linux coreutils gnugrep]}" \
-      --set JAVA_HOME "${jdk17}" \
-      --set ES_JAVA_HOME "${jdk17}"
-
-    wrapProgram $out/bin/elasticsearch-plugin \
+      wrapProgram $out/bin/elasticsearch \
+        --prefix PATH : "${lib.makeBinPath [util-linux coreutils gnugrep]}" \
         --set JAVA_HOME "${jdk17}" \
         --set ES_JAVA_HOME "${jdk17}"
 
-    runHook postInstall
-  '';
+      wrapProgram $out/bin/elasticsearch-plugin \
+          --set JAVA_HOME "${jdk17}" \
+          --set ES_JAVA_HOME "${jdk17}"
 
-  passthru = {enableUnfree = true;};
+      runHook postInstall
+    '';
 
-  meta = with lib; {
-    description = "Open Source, Distributed, RESTful Search Engine";
-    sourceProvenance = with sourceTypes; [
-      binaryBytecode
-      binaryNativeCode
-    ];
-    license = licenses.elastic20;
-    platforms = platforms.unix;
-  };
-}
+    passthru = {enableUnfree = true;};
+
+    meta = with lib; {
+      description = "Open Source, Distributed, RESTful Search Engine";
+      sourceProvenance = with sourceTypes; [
+        binaryBytecode
+        binaryNativeCode
+      ];
+      license = licenses.elastic20;
+      platforms = platforms.unix;
+    };
+  }
