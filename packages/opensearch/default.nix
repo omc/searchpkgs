3342bd64f1bad09640ff29997d4834c2c1c441b4
Nick Zadrozny <nick@onemorecloud.com>
2024-06-12 16:16:52 -0500

    light port of the packaging from ES to OS

--- packages/opensearch/default.nix
+++ packages/opensearch/default.nix
@@ -3,17 +3,61 @@
   version,
   url,
   sha256,
+  lib,
   stdenv,
   fetchurl,
-}:
-stdenv.mkDerivation {
-  inherit pname version;
-  src = fetchurl {
-    inherit url sha256;
-  };
-  buildPhase = '''';
-  installPhase = ''
-    mkdir $out
-    mv * $out
-  '';
-}
+  jdk17,
+  util-linux,
+  zlib,
+  makeWrapper,
+  coreutils,
+  gnugrep,
+  autoPatchelfHook,
+  libxcrypt-legacy,
+  fixDarwinDylibNames,
+  sd,
+}: let
+  # applyPatch = false;
+in
+  stdenv.mkDerivation {
+    inherit pname version;
+    src = fetchurl {
+      inherit url sha256;
+    };
+
+    # patches = lib.optionals applyPatch [];
+
+    # postPatch = ''
+    # '';
+
+    nativeBuildInputs =
+      [makeWrapper sd]
+      ++ lib.optional stdenv.hostPlatform.isLinux autoPatchelfHook
+      ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
+
+    buildInputs = [jdk17 util-linux zlib libxcrypt-legacy];
+    runtimeDependencies = [zlib];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir $out
+      cp -R bin config lib modules plugins $out
+
+      # temporarily exclude some plugins
+      # tbd
+
+      chmod +x $out/bin/*
+
+      wrapProgram $out/bin/opensearch \
+        --prefix PATH : "${lib.makeBinPath [util-linux coreutils gnugrep]}" \
+        --set JAVA_HOME "${jdk17}" \
+        --set OPENSEARCH_JAVA_HOME "${jdk17}"
+
+      wrapProgram $out/bin/opensearch-plugin \
+          --set JAVA_HOME "${jdk17}" \
+          --set OPENSEARCH_JAVA_HOME "${jdk17}"
+
+      runHook postInstall
+    '';
+  }
